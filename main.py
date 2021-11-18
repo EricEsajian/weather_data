3cd5c2d9f080a5f13403f8c27449d64d244cfddf
Eric Esajian <quantumprophetdeveloper@gmail.com>
2021-11-18 08:50:13 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(row[1])
+#     print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+# print(type(data))
+# print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+#
+# #Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+
+#Get Data in Rows
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temp = int(monday.temp)
+# monday_temp_F = monday_temp * 9/5 + 32
+# print(monday_temp_F)
+
